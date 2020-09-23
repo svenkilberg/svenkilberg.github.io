1a5bcf82edf58cd8b392e88fb06090dc9669b168
Sven Kilberg <sven.kilberg@gmail.com>
2020-09-23 11:10:08 +0200

    Repositories added using fetch

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+let gitHubReposContainer = document.getElementById('gitHubReposContainer');
+
+fetch('https://api.github.com/users/svenkilberg/repos')
+.then(response => response.json())
+.then(json => {    
+    console.log(json);
+    drawRepos(json);
+})
+
+function drawRepos(repoList) {
+    
+    for (let i = 0; i < repoList.length; i++) {
+    let gitHubReposHtml = `
+    <a href="${repoList[i].html_url}" target="_blank"><div class="repo">
+    <h6>${repoList[i].name}</h6>
+    <p>${repoList[i].description}</p>
+    </div>
+    </a>`.trim();
+
+    document.getElementById('gitHubReposContainer').insertAdjacentHTML('beforeend', gitHubReposHtml);
+
+    }
+}
